bb61c27b5593f1282bb07843d1f2c5f85d5c5219
LeBriCodeur <52689896+LeBriCodeur@users.noreply.github.com>
2019-11-24 20:02:45 +0100

    Mise en ligne

---
+++ MCF - ShutdownApps/MCF - ShutdownApps/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace MCF___ShutdownApps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.EditHeure = new System.Windows.Forms.TextBox();
+            this.EditMinute = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 118);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Minute :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 80);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Heure : ";
+            // 
+            // EditHeure
+            // 
+            this.EditHeure.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(85)))), ((int)(((byte)(95)))));
+            this.EditHeure.Location = new System.Drawing.Point(245, 77);
+            this.EditHeure.Name = "EditHeure";
+            this.EditHeure.Size = new System.Drawing.Size(114, 22);
+            this.EditHeure.TabIndex = 1;
+            this.EditHeure.Text = "0";
+            // 
+            // EditMinute
+            // 
+            this.EditMinute.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(85)))), ((int)(((byte)(95)))));
+            this.EditMinute.Location = new System.Drawing.Point(245, 115);
+            this.EditMinute.Name = "EditMinute";
+            this.EditMinute.Size = new System.Drawing.Size(114, 22);
+            this.EditMinute.TabIndex = 2;
+            this.EditMinute.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(134, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(169, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Arrêt programmé du PC";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(85)))), ((int)(((byte)(95)))));
+            this.button1.Location = new System.Drawing.Point(12, 161);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 60);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Valider";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(85)))), ((int)(((byte)(95)))));
+            this.button2.Location = new System.Drawing.Point(256, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(168, 60);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Aucun arrêt";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(134, 42);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Arrêt à :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(100)))), ((int)(((byte)(120)))));
+            this.ClientSize = new System.Drawing.Size(436, 233);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.EditMinute);
+            this.Controls.Add(this.EditHeure);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MCF - ShutdownApps";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox EditHeure;
+        private System.Windows.Forms.TextBox EditMinute;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MCF - ShutdownApps/MCF - ShutdownApps/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace MCF___ShutdownApps
+{
+    public partial class Form1 : Form
+    {
+        int heure;
+        int minute;
+        int resultat;
+        bool valideShutdown;
+        string msg;
+        int newHeure;
+        int newMinute;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime actualDate = DateTime.Now;
+            string[] split = actualDate.ToString().Split(new Char[] { ' ', '/' });
+            string actualHeure = split[3];
+            string[] NewSplit = actualHeure.Split(':');
+            newHeure = Convert.ToInt32(NewSplit[0]);
+            newMinute = Convert.ToInt32(NewSplit[1]);
+            valideShutdown = true;
+            msg = "Opération validé !\nMerci d'avoir urilisé :\nMCF - ShutdownApps";
+
+
+            if (EditHeure.Text.Trim() != "")
+            {
+                heure = Convert.ToInt32(EditHeure.Text.Trim());
+            }
+            if (EditMinute.Text.Trim() != "")
+            {
+                minute = Convert.ToInt32(EditMinute.Text.Trim());
+            }
+
+            if (minute != 0)
+            {
+                if (minute >= 60)
+                {
+                    valideShutdown = false;
+                    msg = "Opération impossible !\nMerci de renseigner les minutes entre 0 et 59";
+                }
+                else
+                {
+                    newMinute += minute;
+                    if (newMinute > 60)
+                    {
+                        newHeure++;
+                        newMinute = newMinute -= 60;
+                    }
+                    minute = minute * 60;
+                }
+            }
+
+            if (heure != 0)
+            {
+                newHeure += heure;
+                heure = heure * 60;
+                heure = heure * 60;
+            }
+
+            MessageBox.Show(msg, "MCF - ShutdownApps");
+            if (valideShutdown)
+            {
+                resultat = heure + minute;
+                Process.Start("shutdown", "/s /f /t " + resultat);
+                string heureShutdown = "Arrêt à : \n" + newHeure + "H" + newMinute + "Min";
+                label4.Text = heureShutdown;
+                button2.Text = "Annuler l'arrêt";
+                button1.Enabled = false;
+                button2.Enabled = true;
+                Properties.Settings.Default.isActive = true;
+                Properties.Settings.Default.heureShut = heureShutdown;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.isActive)
+            {
+                button2.Text = "Annuler l'arrêt";
+                button1.Enabled = false;
+                label4.Text = Properties.Settings.Default.heureShut;
+            }
+            else
+            {
+                button2.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.isActive)
+            {
+                Process.Start("shutdown", "/a");
+                button2.Text = "Aucun arrêt";
+                label4.Text = "Arrêt à : ";
+                button1.Enabled = true;
+                button2.Enabled = false;
+                Properties.Settings.Default.isActive = false;
+                Properties.Settings.Default.heureShut = string.Empty;
+                MessageBox.Show("Vous venez d'annuler l'arrêt programmé.");
+            }
+            else
+            {
+                MessageBox.Show("Aucun arrêt programmé n'est en cours");
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Properties.Settings.Default.Save();
+        }
+    }
+}
